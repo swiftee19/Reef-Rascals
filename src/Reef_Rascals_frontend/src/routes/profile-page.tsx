2b4249bb02b1ffbb630f0ba44012f01d8694ecaf
Nicholas Irvin Suhendi <nicholasirvin123@gmail.com>
2024-02-22 17:57:00 +0700

    added document title to Profile Page

--- src/Reef_Rascals_frontend/src/routes/profile-page.tsx
+++ src/Reef_Rascals_frontend/src/routes/profile-page.tsx
@@ -1,7 +1,12 @@
 import Sidebar from "../components/sidebar";
 import "../scss/pages/profile-page.scss";
+import {useEffect} from "react";
 
 export default function ProfilePage() {
+    useEffect(()=>{
+        document.title = "Reef Rascals | Profile"
+    }, [])
+
     return (
         <>
             <div className={"main-container"}>
