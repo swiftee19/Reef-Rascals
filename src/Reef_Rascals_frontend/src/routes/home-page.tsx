daf4e63272b28cadd85267450768d847cef7fc87
Jessica Ryan <jessicaryan0419@gmail.com>
2024-02-23 11:23:31 +0700

    Added new sidebar and font.scss
    Used in homepage (sidebar-nav.tsx)

--- src/Reef_Rascals_frontend/src/routes/home-page.tsx
+++ src/Reef_Rascals_frontend/src/routes/home-page.tsx
@@ -1,7 +1,11 @@
+import SidebarNav from "../components/sidebar-nav";
+
+
 export default function Home() {
     return (
         <>
-            <div>
+            <div className={"main-container"}>
+                <SidebarNav />
                 Home
             </div>
         </>
