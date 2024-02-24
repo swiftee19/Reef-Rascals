a74ed6211d7e0fb03e65b1906001749bcbcad68c
Nicholas Irvin Suhendi <nicholasirvin123@gmail.com>
2024-02-24 12:49:39 +0700

    Update Routing in Error Page and Sidebar

--- src/Reef_Rascals_frontend/src/error-page.tsx
+++ src/Reef_Rascals_frontend/src/error-page.tsx
@@ -1,14 +1,28 @@
 import { useRouteError } from "react-router";
 import "./scss/pages/error-page.scss";
+import {useEffect, useState} from "react";
 
 export default function ErrorPage() {
-    const error = useRouteError();
-    console.error(error);
+    // const error = useRouteError();
+    // console.error(error);
+
+    const [canisterId, setCanisterId] = useState("")
+
+    useEffect(() => {
+        const currentRoute = window.location.href;
+        // console.log(currentRoute)
+        const routeSplit = currentRoute.split("?")
+        // console.log(routeSplit)
+        const tempCanisterId = routeSplit[1];
+        setCanisterId("?"+tempCanisterId)
+    }, []);
 
     return (
         <div id="error-page" className={"error-page"}>
             <h1>Oops!</h1>
             <p>You have traveled to unknown routes, my friend.</p>
+            <p>But don't worry, I'll guide you back to safety.</p>
+            <a href={"/" + canisterId}>Go back to safety</a>
         </div>
     );
 }
