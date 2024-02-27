2ae199d59c3ebca5a1a04f6723de34424e8621ab
alexander ivan <sandhunter30@gmail.com>
2024-02-27 22:42:25 +0700

    [Fix] Collide

--- src/Reef_Rascals_frontend/src/routes/match-page.tsx
+++ src/Reef_Rascals_frontend/src/routes/match-page.tsx
@@ -1,10 +1,10 @@
 import SidebarNav from "../components/sidebar-nav";
-import match from "../../../declarations/matchmaking"
+import {matchmaking} from "../../../declarations/matchmaking"
 
 export default function MatchPage() {
 
     function searchForMatch() {
-        let users = match.matchmaking.getAllUser();
+        let users = matchmaking.getAllUser();
         console.log(users);
     }
 
@@ -12,6 +12,9 @@ export default function MatchPage() {
         <>
             <div className={"main-container"}>
                 <SidebarNav />
+                <button onClick={searchForMatch}>Search</button>
+                <button onClick={searchForMatch}>Search</button>
+                <button onClick={searchForMatch}>Search</button>
                 <button onClick={searchForMatch}>Search</button>
             </div>
         </>
