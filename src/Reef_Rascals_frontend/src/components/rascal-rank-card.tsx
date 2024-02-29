6e49f92db3e15025b9067ae92bd2ad2e8f09d954
Jessica Ryan <jessicaryan0419@gmail.com>
2024-02-29 12:05:42 +0700

    Fix marketplace page

--- src/Reef_Rascals_frontend/src/components/rascal-rank-card.tsx
+++ src/Reef_Rascals_frontend/src/components/rascal-rank-card.tsx
@@ -5,35 +5,41 @@ import RarityLabel from "./rarity-label";
 export default function RascalRankCard({ index, rascal }: { index: number, rascal: Rascal }) {
   return (
     <div className={styles.cardContainer}>
-      <h1>{index + 1}</h1>
-
-      <img className={styles.rascalImg} src={rascal.imageUrl} alt={rascal.name}/>
-
-      <div className={styles.cardDetail}>
-        <h2>{rascal.name}</h2>
-        <span className={styles.cardPrice}>
-          <img src="/favicon.ico" alt="" />
-          <p>0.111</p>
-        </span>
+      <div className={styles.rankNumber}>
+        <h1>{index + 1}</h1>
       </div>
 
-      <div className={styles.cardStats}>
-        <div>
-          <p className={styles.atkLbl}>ATK</p>
-          <p>{rascal.attack.toString()}</p>
-        </div>
-        <div>
-          <p className={styles.hpLbl}>HP</p>
-          <p>{rascal.health.toString()}</p>
-        </div>
-        <div>
-          <p className={styles.spdLbl}>SPD</p>
-          <p>{rascal.speed.toString()}</p>
-        </div>
+      <div className={styles.rascalImg}>
+        <img  src={rascal.imageUrl} alt={rascal.name}/>
       </div>
+
+      <section className={styles.cardAttribute}>
+        <div className={styles.cardDetail}>
+          <h2>{rascal.name}</h2>
+          <span className={styles.cardPrice}>
+            <img src="/favicon.ico" alt="" />
+            <p>0.111</p>
+          </span>
+        </div>
+
+        <div className={styles.cardStats}>
+          <div>
+            <p className={styles.atkLbl}>ATK</p>
+            <p>{rascal.attack.toString()}</p>
+          </div>
+          <div>
+            <p className={styles.hpLbl}>HP</p>
+            <p>{rascal.health.toString()}</p>
+          </div>
+          <div>
+            <p className={styles.spdLbl}>SPD</p>
+            <p>{rascal.speed.toString()}</p>
+          </div>
+        </div>
+      </section>
 
       <div className={styles.rarityLabel}>
-        <RarityLabel rarity={rascal.rarity} />
+        <RarityLabel rarity={rascal.rarity}/>
       </div>
     </div>
   )
