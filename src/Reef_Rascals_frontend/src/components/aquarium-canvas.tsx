c8d5a9b2d09b93360f8fac5b37ccf087f4557ac4
Jessica Ryan <jessicaryan0419@gmail.com>
2024-03-01 12:17:01 +0700

    Canvas aquarium

---
+++ src/Reef_Rascals_frontend/src/components/aquarium-canvas.tsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useRef } from 'react';
+import { Rascal } from '../types/rascal';
+
+interface AquariumCanvasProps {
+  rascals: Rascal[];
+}
+
+const AquariumCanvas: React.FC<AquariumCanvasProps> = ({ rascals }: AquariumCanvasProps) => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const rascalSize = window.innerWidth * 0.06;
+  let positions: { x: number; y: number; dx: number; dy: number; flip: boolean }[] = [];
+
+  useEffect(() => {
+    positions = rascals.map(() => ({
+      x: Math.random() * (window.innerWidth - rascalSize * 2) + rascalSize,
+      y: Math.random() * (window.innerHeight - rascalSize * 2) + rascalSize,
+      dx: Math.random() * 1 + 0.2,
+      dy: Math.random() * 1 + 0.2,
+      flip: false,
+    }));
+  }, [rascals]);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas?.getContext('2d');
+
+    if (!canvas || !context || rascals.length === 0) {
+      return;
+    }
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    const animate = () => {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+
+      rascals.forEach((rascal, index) => {
+        const { x, y, dx, dy, flip } = positions[index];
+
+        const rascalImg = new Image();
+        rascalImg.src = rascal.imageUrl;
+
+        context.save();
+        if (flip) {
+          context.scale(-1, 1);
+          context.drawImage(rascalImg, -x - rascalSize, y - rascalSize, rascalSize * 2, rascalSize * 2);
+        } else {
+          context.drawImage(rascalImg, x - rascalSize, y - rascalSize, rascalSize * 2, rascalSize * 2);
+        }
+        context.restore();
+
+        positions[index] = {
+          x: x + dx,
+          y: y + dy,
+          dx,
+          dy,
+          flip,
+        };
+
+        if (x + dx > canvas.width - rascalSize || x + dx < rascalSize) {
+          positions[index].dx = -dx;
+          positions[index].flip = !positions[index].flip;
+        }
+        if (y + dy > canvas.height - rascalSize || y + dy < rascalSize) {
+          positions[index].dy = -dy;
+        }
+      });
+
+      window.requestAnimationFrame(animate);
+    };
+
+    animate();
+
+    const directionChangeInterval = setInterval(() => {
+      positions.forEach((position) => {
+        const randomDirection = Math.random();
+        if (randomDirection < 0.5) {
+          position.dx = -position.dx;
+          position.flip = !position.flip;
+        } else {
+          position.dy = -position.dy;
+        }
+      });
+    }, Math.random() * 5000 + 5000);
+
+    return () => clearInterval(directionChangeInterval);
+  }, [rascals, positions]);
+
+  return <canvas ref={canvasRef} style={{ position: 'fixed', top: 0, left: 0 }} />;
+};
+
+export default AquariumCanvas;
--- src/Reef_Rascals_frontend/src/routes/aquarium-page.tsx
+++ src/Reef_Rascals_frontend/src/routes/aquarium-page.tsx
@@ -1,21 +1,64 @@
+import AquariumCanvas from "../components/aquarium-canvas";
 import SidebarNav from "../components/sidebar-nav";
 import SlideWoodBtn from "../components/slide-wood-btn";
 import styles from "../scss/pages/aquarium-page.module.scss";
+import { Rarity, Rascal, RascalType } from "../types/rascal";
 
 export default function AquariumPage(){
+
+    const rascal1: Rascal = new Rascal(
+        "#10070111730",
+        "Circus Clio",
+        2,
+        "/rascals/circus-clio.png",
+        RascalType.Fearless,
+        Rarity.Common,
+        20,
+        10,
+        30,
+        "whbpg-wktkv-qm2ea-l545d-ztrdc-ekeci-r4o7y-jiobt-b54l4-534x7-lae"
+    )
+
+    const rascal2: Rascal = new Rascal(
+        "#10070111730",
+        "Axolberry",
+        2,
+        "/rascals/axolberry.png",
+        RascalType.Fearless,
+        Rarity.Common,
+        20,
+        10,
+        30,
+        "whbpg-wktkv-qm2ea-l545d-ztrdc-ekeci-r4o7y-jiobt-b54l4-534x7-lae"
+    )
+
+    const rascal3: Rascal = new Rascal(
+        "#10070111730",
+        "Twinkler",
+        2,
+        "/rascals/twinkler.png",
+        RascalType.Fearless,
+        Rarity.Common,
+        20,
+        10,
+        30,
+        "whbpg-wktkv-qm2ea-l545d-ztrdc-ekeci-r4o7y-jiobt-b54l4-534x7-lae"
+    )
+
+    const rascals = [rascal1, rascal2, rascal3];
+
     return (
         <>
             <SidebarNav/>
             <div className={styles.mainContainer}>
-                <div className={styles.aquariumContainer}>
-                    <footer className={styles.aquariumBottom}>
-                        <img src="/wood-round.png" alt="" />
-                    </footer>
-                    <header className={styles.aquariumTop}>
-                        <SlideWoodBtn/>
-                    </header>
-                </div>
-            </div>
+                <AquariumCanvas rascals={rascals}/>
+                <header className={styles.aquariumTop}>
+                    <SlideWoodBtn/>
+                </header>
+                <footer className={styles.aquariumBottom}>
+                    <img src="/wood-round.png" alt="" />
+                </footer>
+        </div>
         </>
     )
 }
